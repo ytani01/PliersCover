40b3e69c7b54b14bd06074d8dadc158390ccee5b
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2020-02-09 22:28:06 +0900

    update

--- pliers_cover.py
+++ pliers_cover.py
@@ -103,6 +103,7 @@ class ytaniFoo(inkex.Effect):
         obj1 = inkex.etree.SubElement(parent,
                                       inkex.addNS('rect', 'svg'),
                                       attribs1)
+
         """
 
         # pattern1
