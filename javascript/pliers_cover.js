fb5becf97733ddfdccdd8755f6bbf9de5a108ee6
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2020-02-16 19:11:51 +0900

    javascript

--- javascript/pliers_cover.js
+++ javascript/pliers_cover.js
@@ -1,12 +1,14 @@
 function gen_svg() {
     var e = document.getElementById("target");
 
-    var svg1 = "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"200mm\" height=\"200mm\" viewBox=\"0 0 200 200\">";
-    var svg2 = "</svg>";
+    var svg_begin = "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"200mm\" height=\"200mm\" viewBox=\"0 0 200 200\">";
+
+    var svg_end = "</svg>";
 
     svg_d = "M 0,0 L 100,0 L 100,100 L 0,100 Z";
+    var svg_path = "<path stroke=\"red\" stroke-width=\"0.1\" fill=\"none\" d=";
 
-    var svg_text = svg1 + svg_d + svg2;
+    var svg_text = svg1 + svg_path + svg_d + svg2;
 
     var blob = new Blob([ svg_txt ], { "type": "text/plain" });
 
