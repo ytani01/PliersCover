4e768724e6bee906b7cd44efa0a06de9b4ba6ebd
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2020-02-16 18:38:54 +0900

    javascript

--- javascript/pliers_cover.js
+++ javascript/pliers_cover.js
@@ -1,3 +1,15 @@
 function gen_svg() {
-    var e = document.getElementById("target")
+    var e = document.getElementById("target");
+
+    var svg1 = "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"200mm\" height=\"200mm\" viewBox=\"0 0 200 200\">";
+    var svg2 = "</svg>";
+
+    svg_d = "M 0,0 L 100,0 L 100,100 L 0,100 Z";
+
+    var svg_text = svg1 + svg_d + svg2;
+
+    var blob = new Blob([ svgtxt ], {"type": "text/plain"});
+
+    document.getElementById("download").href = window.URL.createObjectURL(blob);
+    window.alert("Finish !");
 }
